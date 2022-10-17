ee3ad0bdfd9f19dff5084b8b774a8cfedab1c455
Dalu <chukwudaluay@gmail.com>
2022-10-16 19:39:27 -0700

    Add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main -prints the size of various types on the computer it is compiled and run on.
+ * main -prints the size of various types on the computer
+ * it is compiled and run on.
  *
  * Return: always 0
  *
