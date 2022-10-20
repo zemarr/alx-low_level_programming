9a2a8862e57fa9a4d5aada9385c10be9a6e29101
Dalu <chukwudaluay@gmail.com>
2022-10-19 18:38:32 -0700

    Add 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet_x1(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
-int isLower(int c);
-int isAlpha(int c);
-int printSign(int n);
-int abs(int);
-int printLastDigit(int);
-void jackBauer(void);
-void timesTable(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
 int add(int, int);
-void printTo98(int n);
+void print_to_98(int n);
 int _putchar(char c);
 #endif
