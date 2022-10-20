519206126acc31c2f7b90ca9644fe9ca92342413
Dalu <chukwudaluay@gmail.com>
2022-10-19 18:15:26 -0700

    Add 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+
 
 /**
  * main - Print a string using puts
@@ -9,6 +10,14 @@
 
 int main(void)
 {
-	printf("_putcher\n");
+	_putchar("_");
+	_putchar("p"); 
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
