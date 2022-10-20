ff81eba70a1c4bd24ea6ed81592356c6eb02df6f
Dalu <chukwudaluay@gmail.com>
2022-10-19 18:18:17 -0700

    Add main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet_x1(void);
+void print_alphabet_x10(void);
+int isLower(int c);
+int isAlpha(int c);
+int printSign(int n);
+int abs(int);
+int printLastDigit(int);
+void jackBauer(void);
+void timesTable(void);
+int add(int, int);
+void printTo98(int n);
+int _putchar(char c);
+#endif
