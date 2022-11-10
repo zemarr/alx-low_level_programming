982401de8566c205431e973945543459836e665c
Dalu <chukwudaluay@gmail.com>
2022-11-10 08:06:44 -0800

    update main.h file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,35 +15,35 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			putchar('0');
+			_putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 99)
-					putchar(' ');
+					_putchar(' ');
 
 				if (prod <= 9)
-					putchar(' ');
+					_putchar(' ');
 
 				if (prod >= 100)
 				{
-					putchar((prod / 100) + '0');
-					putchar(((prod / 10)) % 10 + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					putchar((prod / 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
 
-				putchar((prod % 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
