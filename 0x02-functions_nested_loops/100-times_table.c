4c6a21a2c9d024c6fb21ef05a4788a7d2d601a9c
Dalu <chukwudaluay@gmail.com>
2022-11-10 07:59:08 -0800

    modify 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,35 +15,35 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar('0');
+			putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 99)
-					_putchar(' ');
+					putchar(' ');
 
 				if (prod <= 9)
-					_putchar(' ');
+					putchar(' ');
 
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					putchar((prod / 100) + '0');
+					putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod / 10) + '0');
+					putchar((prod / 10) + '0');
 				}
 
-				_putchar((prod % 10) + '0');
+				putchar((prod % 10) + '0');
 			}
 
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
