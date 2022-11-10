ff1d623488c435bee00da0c93d033ada1f06922f
Dalu <chukwudaluay@gmail.com>
2022-11-10 15:55:09 -0800

    Compile 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,13 @@ void more_numbers(void)
 {
 	int num, count;
 
-	for (count = 0; num <= 14; num++)
+	for (count = 0; count <= 14; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-				_putchar((num % 10) + '0');
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
