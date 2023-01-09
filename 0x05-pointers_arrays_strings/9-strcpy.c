7cc3806ab45b0337805d764163235cbd33d96d3c
Chukwudalu Ayika <54899981+zemarr@users.noreply.github.com>
2023-01-09 17:59:59 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,16 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+int inc = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
 
-	return (dest);
+return (dest);
 }
